2fb1429241f23a1325b7fa2f90bc294903912a80
Rambabu Anandharaman <rambabu.anandharaman@urjanet.com>
2018-05-12 11:36:30 +0530

    Initial Project Setup done.

---
+++ flipkartbackend/src/main/java/com/thechangemakers/flipkartbackend/App.java
@@ -0,0 +1,13 @@
+package com.thechangemakers.flipkartbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
